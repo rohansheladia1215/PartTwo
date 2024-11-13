7f9790c21b21446adb1300dc99960113d26913ce
rohansheladia1215 <rohan.sheladia@gmail.com>
2024-11-13 09:29:17 -0800

    counting threads

--- PartTwoThreadsCode/src/Main.java
+++ PartTwoThreadsCode/src/Main.java
@@ -1,15 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(1000);
+
+        List<Future<Long>> futures = new ArrayList<>();
+        for(int i=0;i<1000;i++)
+        {
+           Callable<Long> task = new Callable<Long>() {
+               @Override
+               public Long call() throws Exception {
+                   long sum = 0;
+                   for(int j=1;j<=1000000;j++)
+                   {
+                       sum++;
+                   }
+                   return sum;
+               }
+           };
+
+            futures.add(fixedThreadPool.submit(task));
         }
+
+        long totalSum =0;
+        for(Future<Long> f: futures)
+        {
+            totalSum+=f.get();
+        }
+
+        System.out.println("Sum is: "+totalSum);
+
     }
 }
